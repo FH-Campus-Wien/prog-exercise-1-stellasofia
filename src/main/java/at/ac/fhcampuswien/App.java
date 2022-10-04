b168312bd3a07150b99d077d2b8c84fa45d28c5c
stellasofia <stellasofiagross@gmail.com>
2022-10-04 21:19:41 +0200

    All Tasks complete +

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -159,16 +159,18 @@ public class App {
     public void transposedNumbers(){
         System.out.print("Number: ");
         int number = scan.nextInt();
-        int digit;
-        int reverse = 0;
 
-        while(number > 0){
-            digit = number % 10;
-            number = number / 10;
-            reverse = reverse * 10 + digit;
+        if (number <= 999){
+            int digit;
+            int reverse = 0;
+
+            while(number > 0){
+                digit = number % 10;
+                number = number / 10;
+                reverse = reverse * 10 + digit;
+            }
+            System.out.println(reverse);
         }
-
-        System.out.println(reverse);
     }
 
 
